4e08a57023b5feda0a1e5aff611195ad24d78bbe
Joseph Clyde Mar <clydemar234@gmail.com>
2024-01-06 23:03:44 +0800

    lesson01: Add basic types

--- lesson01/build/js/main.js
+++ lesson01/build/js/main.js
@@ -1,4 +1,11 @@
 "use strict";
+let a = 12;
+let b = '6';
+let c = 2;
+let d = false;
+console.log(a / c);
+console.log(a * c);
+let username = 'josephclyde';
 for (let i = 0; i < 5; i++) {
-    console.log(`I am Joseph Clyde ${i}`);
+    console.log(`I am ${username} ${(i + 1) * 10}`);
 }
--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -1,3 +1,15 @@
+let a: number = 12;
+let b: string = '6';
+let c: number = 2;
+let d: boolean = false;
+
+
+console.log(a / c);
+console.log(a * c);
+
+let username = 'josephclyde';
 for(let i=0; i<5; i++) {
-    console.log(`I am Joseph Clyde ${i}`);
+    console.log(`I am ${username} ${(i + 1) * 10}`);
 }
+
+
