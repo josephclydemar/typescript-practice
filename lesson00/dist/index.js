58b177e839a762a94b2649f23ce7eb242b0ccfb3
Joseph Clyde Mar <clydemar234@gmail.com>
2024-01-23 12:49:58 +0800

    typescript with node

---
+++ lesson00/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function add(x, y) {
+    return `Sum is ${x + y}`;
+}
+console.log(add(5, 7));
---
+++ lesson00/src/index.ts
@@ -0,0 +1,5 @@
+function add(x: number, y: number): string {
+    return `Sum is ${x + y}`;
+}
+
+console.log(add(5, 7));
