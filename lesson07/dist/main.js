49cf82c2f8d1974b5bbe4bfbfecc38f3488b3322
Joseph Clyde Mar <clydemar234@gmail.com>
2024-04-01 01:52:41 +0800

    Update lesson07

--- lesson07/dist/main.js
+++ lesson07/dist/main.js
@@ -1,11 +1,129 @@
 "use strict";
 class Citizen {
-    constructor(name, age, gender, language) {
+    constructor(name, age, gender, language, hobby = 'Play Chess') {
         this.name = name;
         this.age = age;
         this.gender = gender;
         this.language = language;
+        this.hobby = hobby;
+    }
+    getAge() {
+        return this.age;
     }
 }
-const x = new Citizen('Michael', 110000000000, 'M', 'Enochian');
+class Foreigner {
+    constructor(name, age, gender, language, hobby = 'Play Mobile Games') {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.language = language;
+        this.hobby = hobby;
+    }
+    getAge() {
+        return this.age;
+    }
+    getHobby() {
+        return this.hobby;
+    }
+}
+class Politician extends Citizen {
+    constructor(name, age, gender, language, title) {
+        super(name, age, gender, language);
+        this.title = title;
+        this.title = title;
+    }
+    getTitle() {
+        return this.title;
+    }
+}
+class Musician extends Foreigner {
+    constructor(name, age, gender, language, instrument) {
+        super(name, age, gender, language);
+        this.instrument = instrument;
+    }
+    getInstrument() {
+        return this.instrument;
+    }
+}
+const w = new Citizen('Michael', 110000000000, 'M', 'Enochian');
+const x = new Foreigner('Zeus', 5200, 'M', 'Greek');
+const y = new Politician('Rodrigo', 78, 'M', 'Bisaya', 'President');
+const z = new Musician('Taylor', 38, 'F', 'English', 'Guitar');
+console.log(w);
+console.log(w.getAge());
 console.log(x);
+console.log(x.getAge());
+console.log(y);
+console.log(y.getTitle(), y.getAge());
+console.log(z);
+console.log(z.getInstrument(), z.getAge());
+class Animal {
+    constructor(name, age, mass) {
+        Animal.count += 1;
+        this.name = name;
+        this.age = age;
+        this.mass = mass;
+    }
+    eat(food) {
+        return `Eating ${food}`;
+    }
+    getAge() {
+        return this.age;
+    }
+    getMass() {
+        return this.mass;
+    }
+}
+Animal.count = 0;
+class Whale extends Animal {
+    constructor(name, age, mass, maxMilkProductionVolumeRate, maxSwimSpeed) {
+        super(name, age, mass);
+        this.maxMilkProductionVolumeRate = maxMilkProductionVolumeRate;
+        this.maxSwimSpeed = maxSwimSpeed;
+    }
+    feedMilkToOffspring(milkVolume) {
+        if (milkVolume > this.maxMilkProductionVolumeRate) {
+            return `Feeding to Offspring ${this.maxMilkProductionVolumeRate}mL of Milk.`;
+        }
+        return `Feeding to Offspring ${milkVolume}mL of Milk.`;
+    }
+    swim(speed) {
+        if (speed > this.maxSwimSpeed) {
+            return `Swimming at speed of ${this.maxSwimSpeed}m/s.`;
+        }
+        return `Swimming at speed of ${speed}m/s.`;
+    }
+}
+class Dog extends Animal {
+    constructor(name, age, mass, maxMilkProductionVolumeRate, maxRunSpeed) {
+        super(name, age, mass);
+        this.maxMilkProductionVolumeRate = maxMilkProductionVolumeRate;
+        this.maxRunSpeed = maxRunSpeed;
+        this.owner = '';
+    }
+    get getOwner() {
+        return this.owner;
+    }
+    set setOwner(owner) {
+        this.owner = owner;
+    }
+    feedMilkToOffspring(milkVolume) {
+        if (milkVolume > this.maxMilkProductionVolumeRate) {
+            return `Feeding to Offspring ${this.maxMilkProductionVolumeRate}mL of Milk.`;
+        }
+        return `Feeding to Offspring ${milkVolume}mL of Milk.`;
+    }
+    run(speed) {
+        if (speed > this.maxRunSpeed) {
+            return `Running at speed of ${this.maxRunSpeed}m/s.`;
+        }
+        return `Running at speed of ${speed}m/s.`;
+    }
+}
+let animals = [
+    new Animal('Claire', 45, 21.41),
+    new Whale('Bubbles', 27, 611.51, 12, 51),
+    new Dog('Orea', 0.92, 1.1, 1.5, 3)
+];
+console.log(animals);
+console.log(Animal.count);
--- lesson07/src/main.ts
+++ lesson07/src/main.ts
@@ -1,16 +1,187 @@
+// Classes
 class Citizen {
-    // name: string;
-    // age: number;
-    // gender: string;
-    // language: string;
-    constructor(public readonly name: string, public readonly age: number, private gender: string, protected language: string) {
-        // this.name = name;
-        // this.age = age;
-        // this.gender = gender;
-        // this.language = language;
+    constructor(
+        public readonly name: string,
+        private age: number,
+        protected readonly gender: string,
+        protected language: string,
+        private hobby: string = 'Play Chess'
+    ) {}
+
+    public getAge(): number {
+        return this.age;
     }
 }
 
-const x: Citizen = new Citizen('Michael',110_000_000_000, 'M', 'Enochian');
+class Foreigner {
+    public readonly name: string;
+    private age: number;
+    protected readonly gender: string;
+    protected language: string;
+    private hobby: string;
+    constructor(name: string, age: number, gender: string, language: string, hobby: string = 'Play Mobile Games') {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.language = language;
+        this.hobby = hobby;
+    }
+    public getAge(): number {
+        return this.age;
+    }
+    public getHobby(): string {
+        return this.hobby;
+    }
+}
+
+
+
+class Politician extends Citizen {
+    constructor(name: string, age: number, gender: string, language: string, private title: string) {
+        super(name, age, gender, language);
+        this.title = title;
+    }
+    public getTitle(): string {
+        return this.title;
+    }
+}
+
+class Musician extends Foreigner {
+    private instrument: string;
+    constructor(name: string, age: number, gender: string, language: string, instrument: string) {
+        super(name, age, gender, language);
+        this.instrument = instrument;
+    }
+    public getInstrument(): string {
+        return this.instrument;
+    }
+}
+
+
+const w: Citizen = new Citizen('Michael', 110_000_000_000, 'M', 'Enochian');
+const x: Foreigner = new Foreigner('Zeus', 5200, 'M', 'Greek');
+const y: Politician = new Politician('Rodrigo', 78, 'M', 'Bisaya', 'President');
+const z: Musician = new Musician('Taylor', 38, 'F', 'English', 'Guitar');
+
+console.log(w);
+console.log(w.getAge())
+
 console.log(x);
+console.log(x.getAge());
+
+console.log(y);
+console.log(y.getTitle(), y.getAge());
+
+console.log(z);
+console.log(z.getInstrument(), z.getAge());
+
+
+
+
+
+// Interfaces
+class Animal {
+    static count: number = 0;
+    public readonly name: string;
+    protected age: number;
+    protected mass: number;
+    constructor(name: string, age: number, mass: number) {
+        Animal.count += 1;
+        this.name = name;
+        this.age = age;
+        this.mass = mass;
+    }
+    eat(food: string): string {
+        return `Eating ${food}`;
+    }
+    public getAge(): number {
+        return this.age;
+    }
+    public getMass(): number {
+        return this.mass;
+    }
+}
+interface Mammal {
+    maxMilkProductionVolumeRate: number;
+    feedMilkToOffspring(volume: number): string;
+}
+
+interface Aquatic {
+    maxSwimSpeed: number;
+    swim(speed: number): string;
+}
+interface Terrestrial {
+    maxRunSpeed: number;
+    run(speed: number): string;
+}
+
+class Whale extends Animal implements Mammal, Aquatic {
+    maxMilkProductionVolumeRate: number;
+    maxSwimSpeed: number;
+    constructor(name: string, age: number, mass: number, maxMilkProductionVolumeRate: number, maxSwimSpeed: number) {
+        super(name, age, mass);
+        this.maxMilkProductionVolumeRate = maxMilkProductionVolumeRate;
+        this.maxSwimSpeed = maxSwimSpeed;
+    }
+    feedMilkToOffspring(milkVolume: number): string {
+        if(milkVolume > this.maxMilkProductionVolumeRate) {
+            return `Feeding to Offspring ${this.maxMilkProductionVolumeRate}mL of Milk.`;
+        }
+        return `Feeding to Offspring ${milkVolume}mL of Milk.`;
+    }
+    swim(speed: number): string {
+        if(speed > this.maxSwimSpeed) {
+            return `Swimming at speed of ${this.maxSwimSpeed}m/s.`;
+        }
+        return `Swimming at speed of ${speed}m/s.`;
+    }
+}
+
+class Dog extends Animal implements Mammal, Terrestrial {
+    maxMilkProductionVolumeRate: number;
+    maxRunSpeed: number;
+    private owner: string;
+    constructor(name: string, age: number, mass: number, maxMilkProductionVolumeRate: number, maxRunSpeed: number) {
+        super(name, age, mass);
+        this.maxMilkProductionVolumeRate = maxMilkProductionVolumeRate;
+        this.maxRunSpeed = maxRunSpeed;
+        this.owner = '';
+    }
+    public get getOwner(): string {
+        return this.owner;
+    }
+    public set setOwner(owner: string) {
+        this.owner = owner;
+    }
+
+    feedMilkToOffspring(milkVolume: number): string {
+        if(milkVolume > this.maxMilkProductionVolumeRate) {
+            return `Feeding to Offspring ${this.maxMilkProductionVolumeRate}mL of Milk.`;
+        }
+        return `Feeding to Offspring ${milkVolume}mL of Milk.`;
+    }
+    run(speed: number): string {
+        if(speed > this.maxRunSpeed) {
+            return `Running at speed of ${this.maxRunSpeed}m/s.`;
+        }
+        return `Running at speed of ${speed}m/s.`;
+    }
+}
+
+
+
+
+let animals: Animal[] = [
+    new Animal('Claire', 45, 21.41),
+    new Whale('Bubbles', 27, 611.51, 12, 51),
+    new Dog('Orea', 0.92, 1.1, 1.5, 3)
+];
+
+
+console.log(animals);
+console.log(Animal.count);
+
+
+
+
 
